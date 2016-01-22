1e3986eb6e00de139066bb148c67a80645ea1654
machinestat <guoshu2004@hotmail.com>
2016-01-21 19:47:00 -0500

    trains multiple logistic regression classifiers

--- LogisticRegression/oneVsAll.m
+++ LogisticRegression/oneVsAll.m
@@ -25,11 +25,13 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 % Run fmincg to obtain the optimal theta
 % This function will return theta and the cost 
-for c = 1:num_labels
-[theta] = fminunc(@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-	initial_theta, options);
-  all_theta(c, :) = theta;
+for c = 1:num_labels;
+    [theta] = ...
+        fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+        initial_theta, options);
+    all_theta(c, :) = theta;
 end;
+
 
 
 % =========================================================================
